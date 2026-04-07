ff79138c1860f4e3ae1cb168475dab230ae1bf8a
Mehta Dhairya <mdhairya12@gmail.com>
2026-04-07 12:12:16 +0530

    db connected successfully and evn.js completed.

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 const app = express();
-const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => { res.send("Hello World") });
-console.log("mongo uri: " + ENV.MONGO_URI);
-console.log("node env: " + ENV.NODE_ENV);
 
-app.listen(ENV.PORT, () => { console.log("Servers is running on port: " + ENV.PORT) });
+
+app.listen(ENV.PORT, () => {
+    console.log("Servers is running on port: " + ENV.PORT)
+    connectDB();
+});
+
